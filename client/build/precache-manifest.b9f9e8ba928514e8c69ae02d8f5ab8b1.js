28f3d6fbccb2529985cc3a9d4a8535ac765c52d6
George <gms1980@icloud.com>
2021-06-28 09:56:40 -0500

    updated db

---
+++ client/build/precache-manifest.b9f9e8ba928514e8c69ae02d8f5ab8b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5337c0ae188f0e1ad6aed6328a8f3bec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "924e3b8e949169ef6b19",
+    "url": "/static/css/main.e7186c00.chunk.css"
+  },
+  {
+    "revision": "854018d24930d8725180",
+    "url": "/static/js/2.7cb55f49.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.7cb55f49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "924e3b8e949169ef6b19",
+    "url": "/static/js/main.4f005f51.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
